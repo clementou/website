901d8a0665902c2b38225191fdee407e5ce8e184
clementou <clement.h.ou@gmail.com>
2024-11-17 23:05:14 -0500

    optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://clementou.com',
+  integrations: [tailwind(), sitemap()],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto'
+  }
 });
